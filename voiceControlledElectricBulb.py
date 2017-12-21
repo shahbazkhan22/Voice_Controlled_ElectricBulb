e30861d16f8a5a4ed3f2823925084208fd24c09a
Shahbaz Khan <mailshahbazkhan@gmail.com>
2017-12-21 16:34:55 +0530

    Upload codes

---
+++ voiceControlledElectricBulb.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  3 22:50:46 2017
+
+@author: shahb
+"""
+
+                                                                                                      
+import speech_recognition as sr  
+import serial
+t=10
+# get audio from the microphone    
+port = serial.Serial('COM9', 9600 , timeout=0)                                                                   
+while(t):
+    t=t-1
+    r = sr.Recognizer()                                                                                   
+    with sr.Microphone() as source:
+        x=input('Do you want to continue?(y/n)')
+        print(x)
+        #port.write('1'.encode())
+        if(x=='y'):                                                                       
+            print("Speak:") 
+            #print("Hello")                                                                                  
+            audio = r.listen(source)
+            #print("Hello")
+            print("Speech recorded")
+            inSound = r.recognize_google(audio)
+            print (inSound)
+            if(inSound=='switch on'):
+                inSound = '0'
+            else:
+                 inSound = '1'   
+            port.write(inSound.encode())
+            if inSound == "break":
+                break
+        else:
+            break
+try:
+    print(r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
