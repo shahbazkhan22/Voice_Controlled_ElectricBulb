ed9c3ff948bd9b7d91d652457afa0bdf77aad8d5
Shahbaz Khan <mailshahbazkhan@gmail.com>
2017-12-23 11:32:37 +0530

    Updates

--- voiceControlledElectricBulb.py
+++ voiceControlledElectricBulb.py
@@ -8,36 +8,33 @@ Created on Sun Dec  3 22:50:46 2017
                                                                                                       
 import speech_recognition as sr  
 import serial
-t=10
-# get audio from the microphone    
+# Opening the serial port 'COM9' with baudrate set to 9600    
 port = serial.Serial('COM9', 9600 , timeout=0)                                                                   
-while(t):
-    t=t-1
-    r = sr.Recognizer()                                                                                   
-    with sr.Microphone() as source:
-        x=input('Do you want to continue?(y/n)')
-        print(x)
-        #port.write('1'.encode())
-        if(x=='y'):                                                                       
-            print("Speak:") 
-            #print("Hello")                                                                                  
-            audio = r.listen(source)
-            #print("Hello")
-            print("Speech recorded")
-            inSound = r.recognize_google(audio)
-            print (inSound)
-            if(inSound=='switch on'):
-                inSound = '0'
-            else:
-                 inSound = '1'   
-            port.write(inSound.encode())
-            if inSound == "break":
-                break
+r = sr.Recognizer()                                                                                   
+with sr.Microphone() as source:
+    x=input('Do you want to continue?(y/n)')
+    #print(x)
+    #port.write('1'.encode())
+    if(x=='y'):                                                                       
+        print("Speak:") 
+        #print("Hello")                                                                                  
+        audio = r.listen(source)
+        #print("Hello")
+        print("Speech recorded")
+        inSound = r.recognize_google(audio)
+        print (inSound)
+        if(inSound=='switch on'):
+            inSound = '0'
         else:
+             inSound = '1'   
+        port.write(inSound.encode())
+        if inSound == "break":
             break
-try:
-    print(r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+    else:
+        break
+#try:
+#    print(r.recognize_google(audio))
+#except sr.UnknownValueError:
+#   print("Could not understand audio")
+#except sr.RequestError as e:
+#    print("Could not request results; {0}".format(e))
